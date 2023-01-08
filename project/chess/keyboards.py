b10a24d022e44c47cb68f0250ea9fab1e52a2a12
Alfi111 <97363588+Alfi111@users.noreply.github.com>
2023-01-08 23:27:54 +0300

    Update keyboards.py

--- project/chess/keyboards.py
+++ project/chess/keyboards.py
@@ -1,16 +1,11 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo, InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.utils.callback_data import CallbackData
 
-web_app = WebAppInfo(url="https://alfi111.github.io/")
+web_app = WebAppInfo(url="https://alfi111.github.io/project/chess/index.html")
 
 keyboard = ReplyKeyboardMarkup(
     keyboard=[
-        [KeyboardButton(text="Сайт", web_app=web_app)]
+        [KeyboardButton(text="Chess game", web_app=web_app)]
     ],
     resize_keyboard=True
-)
-
-cb = CallbackData('btn', 'action')
-key = InlineKeyboardMarkup(
-    inline_keyboard=[[InlineKeyboardButton('Оплата', callback_data='btn:buy')]]
 )
