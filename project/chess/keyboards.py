7041b74fcf039785046f752440109b62c32662ef
Alfi111 <97363588+Alfi111@users.noreply.github.com>
2023-01-08 23:41:27 +0300

    Update keyboards.py

--- project/chess/keyboards.py
+++ project/chess/keyboards.py
@@ -5,7 +5,7 @@ web_app = WebAppInfo(url="https://alfi111.github.io/project/chess/index.html")
 
 keyboard = ReplyKeyboardMarkup(
     keyboard=[
-        [KeyboardButton(text="Chess game", web_app=web_app)]
+        [KeyboardButton(text="Play Chess Game", web_app=web_app)]
     ],
     resize_keyboard=True
 )
