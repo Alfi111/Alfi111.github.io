5f0af6b38b87820f7b513ae39f8032abb1987c2d
Alfi111 <97363588+Alfi111@users.noreply.github.com>
2023-01-09 13:16:07 +0300

    Update script.js

--- project/chess/script.js
+++ project/chess/script.js
@@ -192,7 +192,7 @@ var makeBestMove = function () {
     board.position(game.fen());
     renderMoveHistory(game.history());
     if (game.game_over()) {
-        alert('Соперник поставил мат!');
+        alert('Партия окончена!');
     }
 };
 
@@ -200,7 +200,7 @@ var makeBestMove = function () {
 var positionCount;
 var getBestMove = function (game) {
     if (game.game_over()) {
-        alert('Соперник поставил мат!');
+        alert('Партия окончена!');
     }
 
     positionCount = 0;
