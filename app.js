a6dffa0ebc399b2262458ab1cc82043288b87355
Alfi111 <97363588+Alfi111@users.noreply.github.com>
2025-11-17 23:01:53 +0300

    Update app.js

--- app.js
+++ app.js
@@ -164,7 +164,7 @@ function setupCartButton() {
     if (cartItems.length > 0) {
         const totalAmount = cartItems.reduce((sum, item) => sum + (item.price * item.count), 0);
         tg.MainButton.setText(`Оплатить ${totalAmount}₽`);
-        tg.MainButton.setParams({ color: '#2481cc' });
+        tg.MainButton.setParams({ color: '#00D200' });
         tg.MainButton.show();
     } else {
         tg.MainButton.hide();
